7003986b456bc0068f3a0f2a623dec402f072bbf
Formacio <formacio@formacio.com>
2022-07-25 17:47:16 +0200

    Initial Commit - Agregadas primeras líneas de código e incorporado Bootstrap

---
+++ test/src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <div className='bg-primary card'>
+        <Header/>
+      </div>
+    </>
+    );
+  }
+  
+  export default App;
